b87a9dc5f0e77399e82dd96cfefc2a681434d059
Brian <brian@brianpc>
2023-05-21 12:42:22 -0500

    db restructured, logs methods and exercises methods restructured

--- index.js
+++ index.js
@@ -12,17 +12,31 @@ app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// const userSchema = new mongoose.Schema({
+//   username: String,
+//   count: Number,
+//   log: [{
+//     description: String,
+//     duration: Number,
+//     date: Date,
+//   }]
+// });
+
+const Schema = mongoose.Schema;
+
 const userSchema = new mongoose.Schema({
-  username: String,
-  count: Number,
-  log: [{
-    description: String,
-    duration: Number,
-    date: Date,
-  }]
+  username: String
 });
 
+const userExercisesSchema= new mongoose.Schema({
+  id_user: { type: Schema.Types.ObjectId, ref: 'User' },
+  description: String,
+  duration: Number,
+  date: Date
+})
+
 const User = mongoose.model('User', userSchema)
+const Exercises = mongoose.model('Exercises', userExercisesSchema)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -45,23 +59,23 @@ app.post('/api/users', (req, res) => {
 
 app.post('/api/users/:_id/exercises', (req, res) => {
   let id = req.params._id
-  // let id = req.body.id
   let description = req.body.description
   let duration = req.body.duration
   let date = req.body.date ? req.body.date : new Date()
-  //let count = {count: count+1}
   let log = {
-    log: {
-      description: description,
-      duration: duration,
-      date: date
-    }
+    id_user: id,
+    description: description,
+    duration: duration,
+    date: date
   }
 
-  User.findByIdAndUpdate(id, {$push: log, count: +1}, {new: true }, function(err, data) {
+  Exercises.create(log, function(err, data) {
     if (err) return console.error(err);
-    // console.log("Datos actualizados: ",JSON.stringify(data))
-    res.json({_id: id, username: data.username, date: new Date(date).toDateString(), duration: duration, description: description})
+    data.populate('id_user', function(err, userData) {
+      if (err) return console.error(err);
+      // Acceder al nombre del usuario a través del campo id_user
+      res.json({_id: id, username: userData.id_user.username, date: new Date(date).toDateString(), duration: duration, description: description})
+    })
   })
 });
 
@@ -70,140 +84,56 @@ app.get('/api/users/:_id/logs', (req, res) => {
   let to = req.query.to ? new Date(req.query.to) : false
   let limit = req.query.limit ? req.query.limit : false
   let id = req.params._id
-  
-  User.findById(id, function (err, data) {
+
+  Exercises.find({id_user: id}).populate('id_user').exec(function (err, data) {
     if (err) return console.error(err);
-
-    let log = data.log
-
-    if(from && to){
-      // console.log("Fecha: ",date[0])
-      let newLog = log.filter(log => { 
-        // console.log("fecha db: ", new Date(log.date).getTime())
-        // console.log("fecha req: ", from.getTime())
-        return new Date(log.date).getTime() >= from.getTime() && new Date(log.date).getTime() <= to.getTime()
-      })
-
-      newLog = limit && limit != "" ? newLog.slice(0,limit) : newLog
-      // console.log("Nuevo log: ",newLog.slice(0,limit))
-      res.json({_id: id, username: data.username, from: from.toDateString(), to: to.toDateString(), count: newLog.length, log: newLog.map(log => {
-        return {description:log.description, duration:log.duration, date:log.date.toDateString()}
-      })})
-    } 
     
-    else {  
-      res.json({_id: id, username: data.username, count: data.log.length, log: log.map(log => {
-        return {description:log.description, duration:log.duration, date:log.date.toDateString()}
-      })})   
+    let log = data.map(log => {
+      return {description: log.description, duration: log.duration, date: new Date(log.date).toDateString()}
+    }).filter(log => { 
+      if (from || to){
+        if (from && to){
+          return new Date(log.date).getTime() >= from.getTime() && new Date(log.date).getTime() <= to.getTime()
+        }
+        if (from){ return new Date(log.date).getTime() >= from.getTime() }
+        if (to){ return new Date(log.date).getTime() <= to.getTime()}
+      } else {
+        return log
+      }
+    })
+    
+    let resp = {
+      _id: id, 
+      username: data[0].id_user.username, 
+      count: log.length, 
+      log: log
     }
-  });
- 
+
+    if (from) { resp.from= from.toDateString() }
+    if (to) { resp.to= to.toDateString() }
+
+    res.json(resp)
+  })
 });
 
-// app.get('/api/users/:_id/logs', (req, res) => {
-//   // let from = req.query.from ? req.query.from : false
-//   // let to = req.query.to ? req.query.to : false
-//   let limit = req.query.limit ? req.query.limit : false
-
-//   const from = new Date("2023-04-29");
-//   const to = new Date("2023-04-30");
-
+// app.post('/api/users/:_id/exercises', (req, res) => {
 //   let id = req.params._id
+//   let description = req.body.description
+//   let duration = req.body.duration
+//   let date = req.body.date ? req.body.date : new Date()
+//   let log = {
+//     log: {
+//       description: description,
+//       duration: duration,
+//       date: date
+//     }
+//   }
 
-//   User.aggregate([
-//     {
-//       $match: {
-//         log: {
-//           $elemMatch: {
-//             date: {
-//               $gte: from,
-//               $lte: to
-//             }
-//           }
-//         }
-//       }
-//     },
-//     {
-//       $project: {
-//         username: 1,
-//         count: 1,
-//         log: 1
-//       }
-//     }
-//   ], function(err, resultados) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.dir(resultados, { depth: null });
-//     }
+//   User.findByIdAndUpdate(id, {$push: log, count: +1}, {new: true }, function(err, data) {
+//     if (err) return console.error(err);
+//     res.json({_id: id, username: data.username, date: new Date(date).toDateString(), duration: duration, description: description})
 //   })
-
-//   // if(from && to){
-//   //   User.aggregate([
-//   //     {
-//   //       $match: {
-//   //         _id: id
-//   //       }
-//   //     },
-//   //     {
-//   //       $match: {
-//   //         log: {
-//   //           $elemMatch: {
-//   //             date: {
-//   //               $gte: from,
-//   //               $lte: to
-//   //             }
-//   //           }
-//   //         }
-//   //       }
-//   //     },
-//   //     {
-//   //       $project: {
-//   //         _id: 1,
-//   //         username: 1,
-//   //         count: 1,
-//   //         log: {
-//   //           $slice: [
-//   //             {
-//   //               $map: {
-//   //                 input: "$log",
-//   //                 as: "item",
-//   //                 in: {
-//   //                   $cond: [
-//   //                     {
-//   //                       $and: [
-//   //                         { $gte: [ "$$item.date", from ] },
-//   //                         { $lte: [ "$$item.date", to ] }
-//   //                       ]
-//   //                     },
-//   //                     "$$item",
-//   //                     null
-//   //                   ]
-//   //                 }
-//   //               }
-//   //             },
-//   //             1
-//   //           ]
-//   //         }
-//   //       }
-//   //     }
-//   //   ]).exec(function(err, resultados) {
-//   //     if (err) {
-//   //       console.log(err);
-//   //     } else {
-//   //       console.dir(resultados, { depth: null });
-//   //     }
-//   //   });
-//   // } else {
-//   //   User.findById(id, function(err, data) {
-//   //     if (err) return console.error(err);
-//   //     console.log(data)
-//   //     res.json({username: data.username, _id: data._id, count: data.count, log: data.log})
-//   //   })
-//   // }
 // });
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
